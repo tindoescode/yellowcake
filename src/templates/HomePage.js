0f3285429624e2f4e36c7e39a12c3ce5fa8b9e4b
Tin-Trung Nguyen <hopthucuatin@gmail.com>
2020-11-03 14:46:22 +0700

    Maximum 9 posts on new post index

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -90,7 +90,7 @@ export const pageQuery = graphql`
     posts: allMarkdownRemark(
       filter: { fields: { contentType: { eq: "posts" } } }
       sort: { order: DESC, fields: [frontmatter___date] }
-      limit: 10
+      limit: 9
     ) {
       edges {
         node {
